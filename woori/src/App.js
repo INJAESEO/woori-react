da86d282f3bbdd525a1c4f9757dcba7395a793fb
INJAESEO <ij_k0413@naver.com>
2021-11-15 21:53:21 +0900

    SignUp 기능 작성중

--- woori/src/App.js
+++ woori/src/App.js
@@ -3,11 +3,20 @@ import Router from "./pages/Router";
 import { createGlobalStyle } from "styled-components";
 import GlobalStyle from "./GlobalStyle";
 
+// 같은 변수값을 공유할 범위 설정
+// export let userContext = React.createContext();
+
 function App() {
+  // let [username, setUsername] = useState("");
+  // let [password, setPassword] = useState("");
+  // let [confirmPassword, setConfirmPassword] = useState("");
   return (
     <>
+      {/* 같은 값을 사용할 구조를 감싼다 */}
       <GlobalStyle />
+      {/* <userContext.Provider> */}
       <Router />
+      {/* </userContext.Provider> */}
     </>
   );
 }
--- woori/src/components/Auth/SignUp/index.jsx
+++ woori/src/components/Auth/SignUp/index.jsx
@@ -1,13 +1,95 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import Footer from "../../common/Footer";
+import axios from "axios";
+// import { userContext } from "../../../App";
+import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
+  // let user = useContext(userContext);
+
+  let navigate = useNavigate()
+
+  // 이름, 비밀번호
+  let [username, setUsername] = useState("")
+  let [password, setPassword] = useState("")
+  let [passwordConfirm, setPasswordConfirm] = useState("")
+  // 오류메시지
+  // let [usernameMessage, setUsernameMessage] = useState("")
+  // let [passwordMessage, setPasswordMessage] = useState("")
+  // let [passwordConfirmMessage, setPasswordConfirmMessage] = useState("")
+  // 유효성 검사
+  let [isName, setIsName] = useState(false)
+  let [isPassword, setIsPassword] = useState(false)
+  
+  
+
+  
+  function onSubmitHandler(e) {
+    // prevent submit
+    e.preventDefault()
+
+    let userData = {
+      username: username,
+      password: password,
+    }
+
+     
+    if (username === "") {
+      alert("아이디를 입력하세요")
+      return false
+    } else if (password === "") {
+      alert("비밀번호를 입력하세요")
+      return false
+    } else if (passwordConfirm === "") {
+      alert("비밀번호 확인을 입력하세요")
+      return false
+    } else if (password !== passwordConfirm) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다")
+      return false
+    } else {
+      axios({
+        method: "POST",
+        url: "/user-api/user/",
+        data: userData,
+      }).then((res) => {
+        console.log(res)
+        // if (res.status === 201) {
+          // }
+        navigate('/createprofile')
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+  
   return (
     <>
-      <h1>hello</h1>
-      <Footer />
+      <h3>우리두리에 잘 오셨어요 회원으로 가입해주세요</h3>
+      <form>
+        <input type="text" placeholder="아이디" onChange={(e) => {
+          console.log(e)
+          setUsername(e.target.value)
+        }
+        } />
+        <input type="password" placeholder="비밀번호" onChange={(e) => {
+          setPassword(e.target.value)
+        }} />
+        <input type="password" placeholder="비밀번호확인" onChange={(e)=>{setPasswordConfirm(e.target.value)}} />
+        <button onClick={onSubmitHandler}>가입완료</button>
+        
+      </form>
     </>
   );
 }
 
 export default SignUp;
+
+
+
+// if (길이 < 7 && 문자X) {
+//   alert("문자 1개 포함하여 7자리 이상 입력하세요")
+//   return false
+// } else {
+//   return true;
+// }
+
