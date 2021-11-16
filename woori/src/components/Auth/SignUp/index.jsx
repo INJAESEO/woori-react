730d710bd820d5cd0a7ba45c3cbb2ea576f7537e
INJAESEO <ij_k0413@naver.com>
2021-11-17 02:18:44 +0900

    기타: 회원가입 유효성검사(alert) & 스타일 적용중

--- woori/src/components/Auth/SignUp/index.jsx
+++ woori/src/components/Auth/SignUp/index.jsx
@@ -3,19 +3,16 @@ import Footer from "../../common/Footer";
 import axios from "axios";
 // import { userContext } from "../../../App";
 import { useNavigate } from "react-router-dom";
+import * as S from "./style";
 
 function SignUp() {
   // let user = useContext(userContext);
   let navigate = useNavigate();
   // 이름, 비밀번호
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState(""); 
   const [passwordConfirm, setPasswordConfirm] = useState(""); 
-  // 오류메시지
-  // let [usernameMessage, setUsernameMessage] = useState("")
-  // let [passwordMessage, setPasswordMessage] = useState("")
-  // let [passwordConfirmMessage, setPasswordConfirmMessage] = useState("")
-  // 유효성 검사
+ 
   const [isName, setIsName] = useState(false);
   const [isPassword, setIsPassword] = useState(false);
 
@@ -30,7 +27,14 @@ function SignUp() {
         console.log(res);
       })
       .then(() => navigate("/login"))
-      .catch((err) => console.log(err));
+      .catch((err) =>
+      // console.dir(err.response.data.password[0])
+      {
+        console.log(err)
+        alert("아이디가 중복됩니다")
+      }
+        
+      );
     return response;
   };
 
@@ -42,6 +46,8 @@ function SignUp() {
       username: username,
       password: password,
     };
+
+    const pattern_eng = /[a-zA-Z]/;
 
     if (username === "") {
       alert("아이디를 입력하세요");
@@ -55,48 +61,49 @@ function SignUp() {
     } else if (password !== passwordConfirm) {
       alert("비밀번호와 비밀번호 확인이 일치하지 않습니다");
       return false;
-    } else {
-      console.log(userData);
-      return signup(userData);
-    }
+    } else if (!pattern_eng.test(username) || !pattern_eng.test(password)) {
+      alert("아이디와 비밀번호에 문자1개 이상 포함해야 합니다")
+      return false;
+    } else if (username.length < 7 || password.length < 7) {
+      alert("아이디와 비밀번호 7자리 이상이어야 합니다")
+      return false;
+    } 
+    // console.log(userData);
+    return signup(userData);
+    
   }
 
   return (
     <>
-      <h3>우리두리에 잘 오셨어요 회원으로 가입해주세요</h3>
-      <form onSubmit={onSubmitHandler}>
-        <input
+      <S.H3>우리두리에 잘 오셨어요!<br />
+        회원으로 가입해주세요</S.H3>
+      <S.Form onSubmit={onSubmitHandler}>
+        <S.Input
           type="text"
-          placeholder="아이디"
+          placeholder="아이디(문자1개포함 7자리 이상)"
           onChange={(e) => {
             setUsername(() => e.target.value);
           }}
         />
-        <input
+        <S.Input
           type="password"
-          placeholder="비밀번호"
+          placeholder="비밀번호(문자1개포함 7자리 이상)"
           onChange={(e) => {
             setPassword(() => e.target.value);
           }}
         />
-        <input
+        <S.Input
           type="password"
           placeholder="비밀번호확인"
           onChange={(e) => {
             setPasswordConfirm(() => e.target.value);
           }}
         />
-        <button type="submit">가입완료</button>
-      </form>
+        <S.Button type="submit">가입완료</S.Button>
+      </S.Form>
     </>
   );
 }
 
 export default SignUp;
 
-// if (길이 < 7 && 문자X) {
-//   alert("문자 1개 포함하여 7자리 이상 입력하세요")
-//   return false
-// } else {
-//   return true;
-// }
---
+++ woori/src/components/Auth/SignUp/style.js
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+export const H3 = styled.h3`
+  margin: 20px;
+  text-align: center;
+`;
+
+export const Input = styled.input`
+  border: 1px solid grey;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Button = styled.button`
+  background-color: #f5f5dc;
+  color: #a35414;
+  font-weight: bold;
+  border: none;
+  width: 196 px;
+  height: 35 px;
+  display: block;
+  margin: 0 auto;
+  border-radius: 5 px;
+  font-size: 16px;
+`;
