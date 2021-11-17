1abd8bc95a74b34234796da8d62b433aa5242b68
INJAESEO <ij_k0413@naver.com>
2021-11-18 02:05:19 +0900

    기타: session 데이터 context API 작성완료, 프로필생성 axios 수정 중

--- woori/src/App.js
+++ woori/src/App.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import Router from "./pages/Router";
 import GlobalStyle from "./GlobalStyle";
-import { CookiesProvider } from "react-cookie";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 // 같은 변수값을 공유할 범위 설정
-// export let userContext = React.createContext();
+export const SetCookieContext = createContext(() => {});
+export const CookieContext = createContext();
 
 function App() {
   // let [username, setUsername] = useState("");
   // let [password, setPassword] = useState("");
   // let [confirmPassword, setConfirmPassword] = useState("");
+
+  const [cookies, setCookie] = useCookies(["token"]);
+
   return (
     <>
       {/* 같은 값을 사용할 구조를 감싼다 */}
       <GlobalStyle>
-        {/* <userContext.Provider> */}
-        <CookiesProvider>
-          <Router />
-        </CookiesProvider>
-        {/* </userContext.Provider> */}
+        <CookieContext.Provider value={cookies}>
+          <SetCookieContext.Provider value={setCookie}>
+            <CookiesProvider>
+              <Router />
+            </CookiesProvider>
+          </SetCookieContext.Provider>
+        </CookieContext.Provider>
       </GlobalStyle>
     </>
   );
--- woori/src/components/Auth/CreateProfile/index.jsx
+++ woori/src/components/Auth/CreateProfile/index.jsx
@@ -1,34 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, createContext} from 'react';
 import axios from 'axios';
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { CookieContext } from '../../../App';
+
+
+
 
 function CreateProfile() {
     
-    const [profileImg, setProfileImg] = useState(null)
+    const [profileImg, setProfileImg] = useState()
+    const [previewImg, setPreviewImg] = useState()
     const [nickname, setNickname] = useState("")
-    const [gender, setGender] = useState("")
+    const selectList = ["man", "woman"]
+    // select에서는 디폴트값이 필요하다
+    const [selected, setSelected] = useState("man")
     const navigate = useNavigate();
 
+    const accessToken = useContext(CookieContext)
+    
+
+   
 
     const createProfile = async (profileData) => {
+        
+
         axios({
             method: 'POST',
-            url: "/user-api/profile",
+            url: "/user-api/profile/",
             data: profileData,
             withCredentials: true,
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": "multipart/form-data"
+            },
         })
             .then((res) => {
             console.log(res)
             }).then(() => navigate("/connect"))
         .catch((err)=>{console.log(err)})
     }
+    
+    function onSelectionHandler (e) {
+        setSelected(e.target.value)
+    }
+
+    function onImgUploadHandler(e) {
+        const imgFile = e.target.files[0];
+        const imgUrl = URL.createObjectURL(imgFile);
+        setProfileImg(imgFile)
+        setPreviewImg(imgUrl)
+    }
 
     function onSubmitHandler(e) {
         e.preventDefault()
 
+        // const imgData = new FormData();
+        // imgData.append("profileImg", profileImg)
+
         let profileData = {
             nickname: nickname,
-            gender: gender,
+            gender: selected,
+            profile_img: profileImg
         }
         if (nickname === "") {
             alert("닉네임을 입력해주세요")
@@ -40,17 +72,13 @@ function CreateProfile() {
     }
 
 
-    function onImgUploadHandler(e) {
-        const imgFile = e.target.files[0];
-        const imgUrl = URL.createObjectURL(imgFile);
-        setProfileImg(imgUrl)
-    }
-
+  
     return (
         <div>
             <h3>연결성공! 프로필을 입력해주세요</h3>
+          
             {/* src속성에 state를 부여하여 미리보기  */}
-            <img src={profileImg} alter="프로필사진입니다."/>
+            <img src={previewImg} alter="프로필사진입니다."/>
             <form onSubmit={onSubmitHandler}>
                 <input type="file"
                     accept="image/*"
@@ -58,9 +86,12 @@ function CreateProfile() {
                     onChange={onImgUploadHandler}
                 ></input>
                 <input type="text" placeholder="닉네임" onChange={(e)=>{setNickname(e.target.value)}}></input>
-                <select onChange={(e)=>{setGender(e.target.value)}}>
-                    <option value="남자">남자</option>
-                    <option value="여자">여자</option>
+                <select onChange={onSelectionHandler}>
+                    {/* <option value="남자">남자</option>
+                    <option value="여자">여자</option> */}
+                    {selectList.map((i) => {
+                        return(<option value={i} key={i}>{i}</option>)
+                    })}
                 </select>
                 <button>가입완료</button>
             </form>
