ec7b3f6015700797ceb9f2e1e88441561f9dc834
HyoRyoung-Yang <js980303@naver.com>
2021-12-19 20:21:47 +0900

    기능: 글쓰기 페이지 생성

---
+++ woori/src/components/New/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../common/Header";
+
+function New() {
+  return (
+    <>
+      <Header />
+      <div></div>
+    </>
+  );
+}
+
+export default New;
