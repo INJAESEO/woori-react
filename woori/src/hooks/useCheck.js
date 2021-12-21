42c4d92e661ee488012af9c679f33b4e5d5d11d8
HyoRyoung-Yang <js980303@naver.com>
2021-12-21 15:34:23 +0900

    기능: 프로필, 커플 확인하는 커스텀 훅 생성

---
+++ woori/src/hooks/useCheck.js
@@ -0,0 +1,46 @@
+import { useContext, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { checkState } from "../utils/atoms";
+import axios from "axios";
+import { CookieContext } from "../App";
+
+export const useCheck = () => {
+  const [check, setCheck] = useRecoilState(checkState);
+  const accessToken = useContext(CookieContext);
+
+  const setCheckStatus = (data) => {
+    if (data?.isProfile && data?.isCouple) {
+      setCheck("isBoth");
+      return;
+    }
+    if (data?.isProfile) {
+      setCheck("isProfile");
+      return;
+    }
+    if (data?.isCouple) {
+      setCheck("isCouple");
+      return;
+    }
+    setCheck(null);
+  };
+
+  const getCheckStatus = async () => {
+    await axios({
+      method: "GET",
+      url: `http://localhost:8000/user-api/check/`,
+      headers: {
+        Authorization: `Token ${accessToken.token}`,
+      },
+    }).then((res) => {
+      setCheckStatus(res.data);
+    });
+  };
+
+  useEffect(() => {
+    if (accessToken) {
+      getCheckStatus();
+    }
+  }, [accessToken]);
+
+  return { check: check };
+};
