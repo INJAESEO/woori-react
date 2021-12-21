7cff3d408676ffed5113cbf6ec35634f68dae4c0
HyoRyoung-Yang <js980303@naver.com>
2021-12-21 18:31:35 +0900

    기타: 지도 수정중

--- woori/src/components/Home/Map/index.jsx
+++ woori/src/components/Home/Map/index.jsx
@@ -2,30 +2,7 @@
 import React, { useEffect, useRef } from "react";
 import * as S from "./style";
 
-export const markerdata = [
-  {
-    title: "고려대학교 서울캠퍼스",
-    lat: 37.5898422803883,
-    lng: 127.031685000726,
-  },
-  {
-    title: "하남돼지집",
-    lat: 37.620842424005616,
-    lng: 127.1583774403176,
-  },
-  {
-    title: "수유리우동",
-    lat: 37.624915253753194,
-    lng: 127.15122688059974,
-  },
-  {
-    title: "맛닭꼬",
-    lat: 37.62456273069659,
-    lng: 127.15211256646381,
-  },
-];
-
-const Map = ({ location, placeList, setPlace }) => {
+const Map = ({ placeList, setPlace }) => {
   const container = useRef(null);
   useEffect(() => {
     mapscript();
@@ -74,38 +51,6 @@ const Map = ({ location, placeList, setPlace }) => {
 
       // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
       searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-
-      // // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
-      kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-        searchDetailAddrFromCoords(
-          mouseEvent.latLng,
-          function (result, status) {
-            if (status === kakao.maps.services.Status.OK) {
-              var detailAddr = !!result[0].road_address
-                ? "<div>도로명주소 : " +
-                  result[0].road_address.address_name +
-                  "</div>"
-                : "";
-              detailAddr +=
-                "<div>지번 주소 : " + result[0].address.address_name + "</div>";
-
-              var content =
-                '<div class="bAddr">' +
-                '<span class="title">법정동 주소정보</span>' +
-                detailAddr +
-                "</div>";
-
-              // 마커를 클릭한 위치에 표시합니다
-              marker.setPosition(mouseEvent.latLng);
-              marker.setMap(map);
-
-              // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-              infowindow.setContent(content);
-              infowindow.open(map, marker);
-            }
-          }
-        );
-      });
     });
 
     // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
@@ -130,11 +75,6 @@ const Map = ({ location, placeList, setPlace }) => {
     function searchAddrFromCoords(coords, callback) {
       // 좌표로 행정동 주소 정보를 요청합니다
       geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
-    }
-
-    function searchDetailAddrFromCoords(coords, callback) {
-      // 좌표로 법정동 상세 주소 정보를 요청합니다
-      geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
     }
 
     // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
